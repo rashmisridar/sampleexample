bfd38d78d59a3726ec48f6035d39a1d6644a1e03
sevenbitsrashmi <rashmi@sevenbits.in>
2023-09-20 12:47:43 +0530

    flutter cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:sampleexample/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
